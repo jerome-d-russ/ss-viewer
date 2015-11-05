4dad7fd9afd5c13262428e9f03e593e62115a393
Jerome Russ <jerome.d.russ@gmail.com>
2015-11-05 16:36:28 -0600

    WIP

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#SS-Viewer
+========
+
+## What is SS-Viewer?
+SS-Viewer is a full-screen bible verse selector that makes it easy for Sunday School classes to share verses on a television or projector.
--- server.js
+++ server.js
@@ -4,67 +4,41 @@ var http = require('https');
 
 app.use(express.static(__dirname));
 
-var bookMenu;
-app.get('/books', function (req, res) {
-  if(bookMenu){
-    console.log('Already cached!');
-    console.log(bookMenu);
-    res.json(JSON.stringify(bookMenu));
-  } else {
-    http.get("https://u6HsSO7H58krEHqMydGDyNx0VwIYsQOeZpYfZhnx:X@bibles.org/v2/versions/eng-NASB/books.js?include_chapters=true", function(res2) {
-      console.log("Got response: " + res2.statusCode);
-      
-      var body = '';
-      res2.on('data', function(chunk) {
-        body += chunk;
-      });
-      res2.on('end', function() {
-        bookMenu = {};
-        var books = JSON.parse(body);
-        console.log(books.response.books.length);
-        for(var i=0;i<books.response.books.length;i++){
-          var book = books.response.books[i];
-          if(book.testament == 'OT' || book.testament == 'NT'){
-            if(!bookMenu[book.testament]){
-              bookMenu[book.testament] = {};
-            }
-            if(!bookMenu[book.testament][book.name]){
-              bookMenu[book.testament][book.name] = {};
-            }
-            bookMenu[book.testament][book.name].order = book.ord;
-            console.log(book.testament);
-            console.log(book.name);
-            console.log(book.chapters.length);
-            for(var j=0;j<book.chapters.length;j++){
-              var chapter = book.chapters[j];
-              console.log(Number.isInteger(chapter.chapter) + " " + chapter.chapter);
-              if(!isNaN(parseFloat(chapter.chapter)) && parseFloat(chapter.chapter) > 0){
-                bookMenu[book.testament][book.name][chapter.chapter] = {};
-                var end = chapter.osis_end.split('.');
-                bookMenu[book.testament][book.name][chapter.chapter].length = end[end.length-1];                
-              }
-            }
-          }
-        }
-        bookArray = [];
-        console.log(JSON.stringify(bookMenu));
-        for(var testament = 0;testament < Object.keys(bookMenu).length; testament++){
-          //bookArray[testament].testament = Object.keys(bookMenu)[testament];
-          for(var book = 0; book < Object.keys(testament).length; book++){
-            //bookArray[testament][bok].name = Object.keys(testament)[book];
-            //bookArray[testament][book][chapter].length = book;
-            for(var chapter = 0; chapter < Object.keys(book).length; chapter++){
-              bookArray[testament][book][chapter] = {};
-              bookArray[testament][book][chapter].length = book.length;
-            }
-          }
-        }
-        res.json(bookArray);
-      });
-    }).on('error', function(e) {
-      console.log("Got error: " + e.message);
+app.get('/passage', function (req, res) {
+
+  if(!req.query.edition || 
+     !req.query.book || 
+     !req.query.chapter || 
+     !req.query.start || 
+     !req.query.end) {
+    res.statusCode = 400;
+    return res.send('Error 400: Need Bible Edition, Book, Chapter, Start, and End.');
+  } 
+
+  var url = "https://u6HsSO7H58krEHqMydGDyNx0VwIYsQOeZpYfZhnx:X@bibles.org/v2/"
+             + req.query.edition + "/passages.js?q[]=" 
+             + req.query.book + "+" 
+             + req.query.chapter + "%3A" 
+             + req.query.start + "-" 
+             + req.query.end;
+
+  console.log(url);
+
+  //https://bibles.org/v2/{{edition}}/passages.js?q[]={{book}}+{{chapter}}%3A{{start}}-{{stop}}
+  http.get(url, function(res2) {
+    console.log("Got response: " + res2.statusCode);
+    
+    var body = '';
+    res2.on('data', function(chunk) {
+      body += chunk;
     });
-  }
+    res2.on('end', function() {
+      var books = JSON.parse(body);
+      res.json(books);
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
 });
 
 app.use(function(req, res, next){
